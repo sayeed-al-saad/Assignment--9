0e509506c1b4641ec0b290b160de1517bc432b7d
sayeed-al-saad <sayeedahmed15162@gmail.com>
2025-01-15 18:03:03 +0600

    hi

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   projectId: "assignment-9-59234",
   storageBucket: "assignment-9-59234.firebasestorage.app",
   messagingSenderId: "1091806894885",
-  appId: "1:1091806894885:web:69e82f6859e0571db858cc"
+  appId: "1:1091806894885:web:69e82f6859e0571db858cc",
 };
 
 // Initialize Firebase
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,22 @@
+import { useLoaderData } from "react-router-dom";
 import Carousel from "./Carousel";
+import Hotels from "./Hotels";
 
 
 
 const Home = () => {
+
+    const categories = useLoaderData();
+    console.log(categories)
+
     return (
         <div>
             <Carousel></Carousel>
+            <div className="grid grid-cols-3">
+                {
+                    categories.map((acategories) => (<Hotels key={acategories.id} categories={acategories} ></Hotels>))
+                }
+            </div>
         </div>
     );
 };
