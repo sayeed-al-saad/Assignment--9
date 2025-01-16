5d94d86330f2788bfcbd34e5d548bfda7ecc0170
sayeed-al-saad <sayeedahmed15162@gmail.com>
2025-01-16 17:56:37 +0600

    5th push

--- src/Pages/Home/Carousel.jsx
+++ src/Pages/Home/Carousel.jsx
@@ -11,23 +11,23 @@ const Carousel = () => {
     love to live</p>
     </div>
     <img
-      src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-      className="w-full" />
+      src="src/assets/image/pexels-donaldtong94-189333.jpg"
+      className="w-full " />
   </div>
   <div id="item2" className="carousel-item w-full">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-      className="w-full" />
+      src="src/assets/image/pexels-jvdm-1457842.jpg"
+      className="w-full " />
   </div>
   <div id="item3" className="carousel-item w-full">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-      className="w-full" />
+      src="src/assets/image/pexels-quark-studio-1159039-2506990.jpg"
+      className="w-full " />
   </div>
   <div id="item4" className="carousel-item w-full">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-      className="w-full" />
+      src="src/assets/image/pexels-pixabay-271618.jpg"
+      className="w-full " />
   </div>
 </div>
 <div className="flex w-full justify-center gap-2 py-2">
